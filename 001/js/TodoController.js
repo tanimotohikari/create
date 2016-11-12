31ccdcefe6faf6c2572cec5c5c6ab386b6aeee8c
sisiQ <hikaritanimoto@hikaritanimoto-no-iMac.local>
2016-11-12 17:06:44 +0900

    [WP]削除機能

--- 001/js/TodoController.js
+++ 001/js/TodoController.js
@@ -3,11 +3,13 @@ function TodoController() {
 
   self.todos = [];
   self.completions = [];
+  index = 0;
 
+  //新しいtodoの追加
   self.create = function() {
     var now = new Date();
     var year = now.getFullYear();
-    var mon = now.getMonth()+1; //１を足すこと
+    var mon = now.getMonth()+1;
     var day = now.getDate();
     var hour = now.getHours();
     var min = now.getMinutes();
@@ -16,28 +18,32 @@ function TodoController() {
     var nowTime = year + '年' + mon + '月' + day + '日' + hour + '時'+ min + '分' + sec + '秒'; 
 
     if (self.newTodo) {
-      self.todos.push({title: self.newTodo, done: false, time: nowTime});
+      self.todos.push({id: index, title: self.newTodo, done: false, time: nowTime});
       var jsonTodoData =[JSON.stringify(self.todos)];
       localStorage.setItem('todos', jsonTodoData);
       self.newTodo = '';
+      index++;
     }
   };
 
+  //localStorageに保存されたtodoを表示する
   function showList() {
     var todos = localStorage.getItem('todos');
     if(todos) {
       todos = JSON.parse(todos);
       for(var i=0, len=todos.length; i<len; i++) {
-        self.todos.push({title: todos[i].title, done: todos[i].done, time: todos[i].time});
+        self.todos.push({id: todos[i].id, title: todos[i].title, done: todos[i].done, time: todos[i].time});
       }
     } else {
       todos = [];
-    }    
+    }
+    index = todos.length;
   }
 
   showList();
 
-  self.archive = function() {
+  //チェックボックスにチェックを入れると自動的に完了リストにtodoが移動する
+  self.check = function() {
     var currentTodo = self.todos;
     self.todos = [];
     angular.forEach(currentTodo, function (todo) {
@@ -49,17 +55,17 @@ function TodoController() {
     });
   };
 
-
-  // self.check = function(done) {
-  //   console.log(done);
-  //   currentTodo = self.todos;
-  //   self.todos = [];
-  //   angular.forEach(currentTodo, function(todo) {
-  //     self.completions.push();
-  //     var jsonTodoData =[JSON.stringify(self.completions)];
-  //     localStorage.setItem('complete', jsonTodoData);
-  //   });
-  // };
-
-  
+  //todoの削除ボタン
+  self.delete = function(todo) {
+    var index;
+    for(var i=0, len=self.todos.length; i<len; i++) {
+      if(todo.id == self.todos[i].id) {
+        index = i;
+       // break;
+      }
+      if(index) {
+        self.todos.splice(index, 1);
+      }
+    }
+  }
 }
