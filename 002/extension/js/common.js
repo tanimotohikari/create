b23049ef27c6f2c43310ea17beb1967b138bdac0
tanimotohikari <tanimoto.hikari@gmail.com>
2016-12-09 09:04:04 +0900

    変更対象範囲拡大

--- 002/extension/js/common.js
+++ 002/extension/js/common.js
@@ -1,3 +1,4 @@
+
 function onButtonClick() {
   target = document.getElementById('output');
   target.innerText = document.forms.id_form1.id_textBox1.value;
@@ -5,6 +6,18 @@ function onButtonClick() {
     var strLength = (target.innerText.match(/￥/g)||[]).length;
     for(var i=0; i<strLength; i++) {
       target.innerText = target.innerText.replace('￥', '/');
+    }
+  }
+  if(_.include(target.innerText, '\\')) {
+    var strLength = (target.innerText.match(/\\/g)||[]).length;
+    for(var i=0; i<strLength; i++) {
+      target.innerText = target.innerText.replace('\\', '/');
+    }
+  }
+  if(_.include(target.innerText, '\/')) {
+    var strLength = (target.innerText.match(/\//g)||[]).length;
+    for(var i=0; i<strLength; i++) {
+      target.innerText = target.innerText.replace('\/', '￥');
     }
   }
 }
