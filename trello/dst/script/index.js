cc8322636d358afc1bae1bd764d7f118a8dbb11a
tanimotohikari <kyupi03401881@gmail.com>
2017-11-16 07:32:26 +0900

    update:master [WIP]header

--- trello/dst/script/index.js
+++ trello/dst/script/index.js
@@ -1,3 +1,11 @@
 $(function(){
-  console.log('test');
+  $('.card').hover(
+    function(){
+      console.log('test');
+      $(this).('.card-foundation').css({'display': 'block'});
+      // $(this).css({
+      //   'display': 'block'
+      // });
+    }
+  );
 });
