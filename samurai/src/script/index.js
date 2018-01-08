b5b1a67318a834e8db0ecd079a8b973a88fea21c
tanimotohikari <kyupi03401881@gmail.com>
2018-01-08 21:41:40 +0900

    add:master [WIP]HTML code

---
+++ samurai/src/script/index.js
@@ -0,0 +1,11 @@
+// $(function(){
+//   $('.card').hover(
+//     function(){
+//       console.log('test');
+//       $('.card-foundation').css({'display': 'block'});
+//       // $(this).css({
+//       //   'display': 'block'
+//       // });
+//     }
+//   );
+// });
